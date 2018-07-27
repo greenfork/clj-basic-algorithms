baceae966a66ae52df5ed150d82e2bd50e2402ae
Dmitry Matveyev <dm.matveyev@protonmail.com>
2018-07-27 13:20:40 +0300

    Create project layout

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clj-basic-algorithms "0.1.0-SNAPSHOT"
+  :description "Basic algorithms written in Clojure with <3"
+  :url "https://github.com/greenfork/clj-basic-algorithms"
+  :license {:name "Unlicense"
+            :url "http://unlicense.org/"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [criterium "0.4.4"]]
+  :main ^:skip-aot clj-basic-algorithms.main)
---
+++ src/clj_basic_algorithms/main.clj
@@ -0,0 +1,10 @@
+(ns clj-basic-algorithms.main)
+
+(defn -main [& args]
+  (println (str "This is a collection of different algorithms.\n"
+                "In order to run distinct algorithms, you should navigate\n"
+                "to the appropriate name space, e.g. using\n"
+                "`(ns clj-basic-algorithms.binary-search-tree.core)`\n"
+                "and run each function by hand examining the source code.\n"
+                "The source code is available at\n"
+                "https://github.com/greenfork/clj-basic-algorithms.")))
