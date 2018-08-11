942aedd7955fa1874a2d08f3a6d924f429f8a914
Dmitry Matveyev <dm.matveyev@protonmail.com>
2018-08-12 01:58:10 +0300

    Add rotations, color flip, insertion and dfs with tests to RB tree

--- src/clj_basic_algorithms/redblack_tree/core.clj
+++ src/clj_basic_algorithms/redblack_tree/core.clj
@@ -6,6 +6,118 @@
 
 (defrecord RB [key left right color])
 
+;;; Rotation
+
+(defn rotate-left [root]
+  (let [new-root (:right root)]
+    (RB. (:key new-root)
+         (RB. (:key root) (:left root) (:left new-root) :red)
+         (:right new-root)
+         :black)))
+
+(defn rotate-right [root]
+  (let [new-root (:left root)]
+    (RB. (:key new-root)
+         (:left new-root)
+         (RB. (:key root) (:right new-root) (:right root) :red)
+         :black)))
+
+(defn rotate-right-left [root]
+  (let [right-tree (:right root)
+        new-root   (:left right-tree)]
+    (RB. (:key new-root)
+         (RB. (:key root) (:left root) (:left new-root) :red)
+         (RB. (:key right-tree) (:right new-root) (:right right-tree) :red)
+         :black)))
+
+(defn rotate-left-right [root]
+  (let [left-tree (:left root)
+        new-root  (:right left-tree)]
+    (RB. (:key new-root)
+         (RB. (:key left-tree) (:left left-tree) (:left new-root) :red)
+         (RB. (:key root) (:right new-root) (:right root) :red)
+         :black)))
+
+;;; Color flip
+
+(defn color-flip [root]
+  (let [lchild (:left  root)
+        rchild (:right root)]
+    (RB. (:key root)
+         (RB. (:key lchild) (:left lchild) (:right lchild) :black)
+         (RB. (:key rchild) (:left rchild) (:right rchild) :black)
+         :red)))
+
+;;; Insertion
+
+(defn balance [root]
+  (let [lchild (:left root)
+        rchild (:right root)]
+    (if (= :red (:color lchild) (:color rchild))
+      [(color-flip root) :please-check]
+      (cond
+        (= :red (:color lchild) (:color (:left lchild)))
+        [(rotate-right root) :please-stop]
+        (= :red (:color lchild) (:color (:right lchild)))
+        [(rotate-left-right root) :please-stop]
+        (= :red (:color rchild) (:color (:right rchild)))
+        [(rotate-left root) :please-stop]
+        (= :red (:color rchild) (:color (:left rchild)))
+        [(rotate-right-left root) :please-stop]))))
+
+(defn- insert-r
+  "Docs."
+  [node value]
+  (cond
+    (nil? (:key node)) [(RB. value
+                             (RB. nil nil nil :black)
+                             (RB. nil nil nil :black)
+                             :red)
+                        :please-check]
+    (< value (:key node))
+    (let [[inserted flag] (insert-r (:left node) value)
+          new-tree        (RB. (:key node) inserted (:right node) (:color node))
+          red-violation?  (= :red (:color node) (:color inserted))]
+      (cond
+        (= :please-stop flag) [new-tree :please-stop]
+        red-violation?        [new-tree :code-red]
+        (= :code-red flag)    (balance new-tree)
+        :else                 [new-tree :please-check]))
+    (> value (:key node))
+    (let [[inserted flag] (insert-r (:right node) value)
+          new-tree        (RB. (:key node) (:left node) inserted (:color node))
+          red-violation?  (= :red (:color node) (:color inserted))]
+      (cond
+        (= :please-stop flag) [new-tree :please-stop]
+        red-violation?        [new-tree :code-red]
+        (= :code-red flag)    (balance new-tree)
+        :else                 [new-tree :please-check]))
+    :else [node :please-stop]))
+
+(defn insert [node value]
+  (let [tree (first (insert-r node value))]
+    (if (= :red (:color tree))
+      (RB. (:key tree) (:left tree) (:right tree) :black)
+      tree)))
+
+(defn insert-multiple
+  ([values]      (insert-multiple nil values))
+  ([node values] (reduce insert node values)))
+
+;;; Searching
+;;;
+;;; Common search algorithms for all binary search trees.
+
+(defn dfs
+  "Depth-first search."
+  [node value]
+  (cond
+    (nil? (:key node))      nil
+    (nil? value)            nil
+    (== value (:key node))  node
+    (<  value (:key node))  (recur (:left node) value)
+    (>  value (:key node))  (recur (:right node) value)))
+
 ;;; Utility functions
 
 (defn RB->map [rb]
@@ -114,24 +226,56 @@
 (s/def ::constant-black-depth #(every? (fn [path] (== (black-depth %)
                                                       (count-black-nodes path)))
                                        (every-path %)))
-;; (def gen-rb-tree #(gen/fmap insert-multiple (gen/vector (gen/int))))
+(def gen-rb-tree #(gen/fmap insert-multiple (gen/vector (gen/int))))
 
 (s/def ::rb-tree (s/nilable
-                  (s/and ::rb-type
+                  (s/and #_::rb-type
                          (s/keys :req-un [::key ::left ::right ::color])
                          ::node
                          ::left-is-smaller
                          ::right-is-bigger)))
-(s/def ::rb-tree-root (s/nilable (s/and ::rb-tree
-                                        #(= :black (:color %))
-                                        ::constant-black-depth)))
+(s/def ::rb-tree-root (s/with-gen
+                        (s/nilable
+                         (s/and ::rb-tree
+                                #(= :black (:color %))
+                                ::constant-black-depth))
+                        gen-rb-tree))
 
 (s/def ::key   (s/nilable int?))
 (s/def ::left  ::rb-tree)
 (s/def ::right ::rb-tree)
 (s/def ::color #{:red :black})
 
+(s/def ::search
+  #(let [tree-values (->> % :args :node RB->keys (filter some?) (into #{}))
+         value       (-> % :args :value)
+         ret-key     (-> % :ret  :key)]
+     (or (and (not (contains? tree-values value))
+              (nil? ret-key))
+         (and (contains? tree-values value)
+              (== value ret-key)))))
+
+(s/fdef dfs
+  :args (s/cat :node ::rb-tree-root :value int?)
+  :ret  ::rb-tree
+  :fn   ::search)
+
+(s/fdef insert
+  :args (s/cat :node ::rb-tree-root :value int?)
+  :ret  ::rb-tree-root
+  :fn   #(some? (dfs (:ret %) (-> % :args :value))))
+
 ;;; Testing
+
+(defn test-it [sym]
+  ((comp not #(contains? % :failure) stest/abbrev-result first stest/check) sym))
+
+(t/deftest rb-tree
+  (t/testing "insertion"
+    (t/is (test-it `insert)))
+  (t/testing "depth-first search"
+    (t/is (test-it `dfs))))
+
 
 (def rb-tree (RB. 13
                   (RB. 8
@@ -175,3 +319,6 @@
                              (RB. nil nil nil :black)
                              :red)
                         :black))
+
+(comment
+  (s/explain ::rb-tree-root (insert-multiple (doto (shuffle (range 15)) println))))
